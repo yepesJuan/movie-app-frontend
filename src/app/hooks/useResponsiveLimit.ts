d8d437d35535f80bb3fef681ea1405718245116c
Juan Yepes <yepes.j@hotmail.com>
2025-03-10 20:24:55 -0400

    feat: test

--- src/app/hooks/useResponsiveLimit.ts
+++ src/app/hooks/useResponsiveLimit.ts
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 
 // Appsync limit is not merely based on items length, but on kb of data.
-// out of scope for this project but we could expirment using a GSI... https://github.com/aws-amplify/amplify-category-api/issues/1408
+// https://github.com/aws-amplify/amplify-category-api/issues/1408
 export function useResponsiveLimit() {
   const [limit, setLimit] = useState(8);
 
